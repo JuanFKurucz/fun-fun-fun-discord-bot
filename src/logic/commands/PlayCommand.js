a4975431bfb08ce68a458eee24f7ee38165eaa90
JuanFKurucz <juanfkurucz@gmail.com>
2019-03-30 19:45:02 -0300

    Fix production 2

--- src/logic/commands/PlayCommand.js
+++ src/logic/commands/PlayCommand.js
@@ -1,7 +1,8 @@
 "use strict";
 
+const path = require("path");
 const Command = require("../Command.js");
-const Chess = require(__dirname + "/../../game/Chess.js");
+const Chess = require(path.join(__dirname, '../..', '/game/Chess.js'));
 
 const games = {};
 
