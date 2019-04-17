c4a42b7c2110c894c80ca02f8c649bfdca169543
JuanFKurucz <juanfkurucz@gmail.com>
2019-04-17 20:58:03 -0300

    No need for this

--- index.js
+++ index.js
@@ -3,15 +3,5 @@
 const Main = require("./src/Main.js");
 const tokenInfo = require("./token.json");
 
-require('http').createServer().listen(3000);
-
-setInterval(function(){
-  require('dns').resolve('www.google.com', function(err) {
-    if (err) {
-      process.exit();
-    }
-  });
-},30000);
-
 var Program = new Main();
 Program.start(tokenInfo["token"]);
