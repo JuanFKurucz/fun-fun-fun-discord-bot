017471e294530e8ab119a6f8206a4a2384f291a6
JuanFKurucz <juanfkurucz@gmail.com>
2019-04-17 21:08:20 -0300

    Keep working

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 const Main = require("./src/Main.js");
 const tokenInfo = require("./token.json");
 
+require('http').createServer().listen(process.env.PORT || 5000);
+
 var Program = new Main();
 Program.start(tokenInfo["token"]);
